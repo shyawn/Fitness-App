9926c6771cfdaeb93472846d51693831b9b007c8
ShawnCsj <csjshawn@gmail.com>
2025-02-26 15:03:25 +0800

    feat: create homepage

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
-import { Image, Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import "../global.css";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function Index() {
   return (
@@ -14,6 +15,42 @@ export default function Index() {
         className="h-full w-full absolute"
         source={require("../assets/images/welcome.png")}
       />
+      <LinearGradient
+        colors={["transparent", "#18181b"]}
+        style={{ width: wp(100), height: hp(70) }}
+        start={{ x: 0.5, y: 0 }}
+        end={{ x: 0.5, y: 0.8 }}
+        className="flex justify-end pb-12 gap-8"
+      >
+        <View className="flex items-center">
+          <Text
+            style={{ fontSize: hp(5) }}
+            className="text-white font-bold tracking-wide"
+          >
+            Best <Text className="text-rose-500">Workouts</Text>
+          </Text>
+          <Text
+            style={{ fontSize: hp(5) }}
+            className="text-white font-bold tracking-wide"
+          >
+            For you
+          </Text>
+        </View>
+
+        <View>
+          <TouchableOpacity
+            style={{ height: hp(7), width: wp(80) }}
+            className="bg-rose-500 flex items-center justify-center mx-auto rounded-full border-[2px] border-neutral-200"
+          >
+            <Text
+              style={{ fontSize: hp(3) }}
+              className="text-white font-bold tracking-widest"
+            >
+              Get Started
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </LinearGradient>
     </View>
   );
 }
