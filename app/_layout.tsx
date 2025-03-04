00de26a4f61b95d8db63f4830b1e8d402971e22d
ShawnCsj <csjshawn@gmail.com>
2025-03-04 14:45:21 +0800

    feat: setup asyncstorage

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,29 @@
 import { Stack } from "expo-router/stack";
 import { Provider } from "react-redux";
 import { store } from "@/store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "../store/store";
+import { ActivityIndicator, View } from "react-native";
 
 export default function RootLayout() {
+  const renderLoading = () => {
+    return (
+      <View>
+        <ActivityIndicator size={"large"} />
+      </View>
+    );
+  };
   return (
     <Provider store={store}>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
+      <PersistGate persistor={persistor} loading={renderLoading()}>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </PersistGate>
     </Provider>
   );
 }
--- store/store.ts
+++ store/store.ts
@@ -1,46 +1,50 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import workoutSlice from "./workoutPlan/workoutSlice";
-import mmkvMiddleware from "./mmkvMiddleware";
-// import {
-//   FLUSH,
-//   PAUSE,
-//   PERSIST,
-//   persistReducer,
-//   persistStore,
-//   PURGE,
-//   REGISTER,
-//   REHYDRATE,
-// } from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+// import workoutSlice from "./workoutPlan/workoutSlice";
+// import mmkvMiddleware from "./mmkvMiddleware";
+import {
+  // FLUSH,
+  // PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  // PURGE,
+  // REGISTER,
+  // REHYDRATE,
+} from "redux-persist";
 // import reduxStorage from "./mmkvMiddleware";
-// import workoutReducer from "./workoutPlan/workoutSlice";
+import workoutReducer from "./workoutPlan/workoutSlice";
 
-// const rootReducer = combineReducers({
-//   theme: workoutReducer,
-// });
-
-// const persistConfig = {
-//   key: "root",
-//   version: 1,
-//   storage: reduxStorage,
-//   timeout: 0,
-// };
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-export const store = configureStore({
-  reducer: {
-    workout: workoutSlice,
-  },
-  // reducer: persistedReducer,
-  // middleware: (getDefaultMiddleware) =>
-  //   getDefaultMiddleware({
-  //     serializableCheck: {
-  //       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-  //     },
-  //   }),
+const rootReducer = combineReducers({
+  workout: workoutReducer,
 });
 
-// export const persistor = persistStore(store);
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+  // version: 1,
+  // storage: reduxStorage,
+  // timeout: 0,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  // reducer: {
+  //   workout: workoutSlice,
+  // },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        ignoredActions: [PERSIST],
+        // ignoredPaths: ["register"],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
