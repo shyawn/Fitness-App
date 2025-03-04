e58ff44b76e6d09f507e08a72fced977b7667038
ShawnCsj <csjshawn@gmail.com>
2025-03-04 14:14:40 +0800

    chore: comment out mmkv code

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ import "./global.css";
 
 import { registerRootComponent } from "expo";
 import { ExpoRoot } from "expo-router";
+import { PersistGate } from "redux-persist/integration/react";
 
 // Must be exported or Fast Refresh won't update the context
+// export function App() {
+//   const ctx = require.context("./app");
+//   return (
+//     <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
+//       <ExpoRoot context={ctx} />
+//     </PersistGate>
+//   );
+// }
 export function App() {
   const ctx = require.context("./app");
   return <ExpoRoot context={ctx} />;
--- store/store.ts
+++ store/store.ts
@@ -1,11 +1,46 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import workoutSlice from "./workoutPlan/workoutSlice";
+import mmkvMiddleware from "./mmkvMiddleware";
+// import {
+//   FLUSH,
+//   PAUSE,
+//   PERSIST,
+//   persistReducer,
+//   persistStore,
+//   PURGE,
+//   REGISTER,
+//   REHYDRATE,
+// } from "redux-persist";
+// import reduxStorage from "./mmkvMiddleware";
+// import workoutReducer from "./workoutPlan/workoutSlice";
+
+// const rootReducer = combineReducers({
+//   theme: workoutReducer,
+// });
+
+// const persistConfig = {
+//   key: "root",
+//   version: 1,
+//   storage: reduxStorage,
+//   timeout: 0,
+// };
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: {
     workout: workoutSlice,
   },
+  // reducer: persistedReducer,
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware({
+  //     serializableCheck: {
+  //       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+  //     },
+  //   }),
 });
+
+// export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
