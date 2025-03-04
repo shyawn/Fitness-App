6a9c3aa1eb76cc96642aa4356d42dd9ca5ab5208
ShawnCsj <csjshawn@gmail.com>
2025-03-04 16:10:42 +0800

    feat: add workout schedule calendar

--- app/(tabs)/myPlan.tsx
+++ app/(tabs)/myPlan.tsx
@@ -2,6 +2,7 @@ import DraggableList from "@/components/DraggableList";
 import { RootState } from "@/store/store";
 import { useState } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
@@ -12,6 +13,8 @@ import { setWorkoutOrder } from "@/store/workoutPlan/workoutSlice";
 import { ScrollView } from "react-native-virtualized-view";
 import { Workout } from "@/types";
 import WorkoutModal from "@/components/WorkoutModal";
+import Schedule from "@/components/Schedule";
+import { StatusBar } from "expo-status-bar";
 
 export default function MyPlan() {
   const [modalVisible, setModalVisible] = useState(false);
@@ -25,39 +28,43 @@ export default function MyPlan() {
   };
 
   return (
-    <View style={styles.container}>
-      {workoutList.length === 0 && (
-        <Text className="font-semibold text-gray-500">
-          Add your workout plan
-        </Text>
-      )}
+    <SafeAreaView className="flex-1 bg-white flex space-y-5" edges={["top"]}>
+      <StatusBar style="auto" />
+      <View style={styles.container}>
+        <Schedule />
+        {workoutList.length === 0 && (
+          <Text className="font-semibold text-gray-500">
+            Add your workout plan
+          </Text>
+        )}
 
-      <TouchableOpacity
-        onPress={toggleModal}
-        className="absolute p-1 right-7 top-10 bg-[#A9A9A9] rounded-full"
-      >
-        <Ionicons name="add" size={wp(5)} color="white" />
-      </TouchableOpacity>
+        <TouchableOpacity
+          onPress={toggleModal}
+          className="absolute p-1 right-7 top-32 mt-2 bg-[#A9A9A9] rounded-full"
+        >
+          <Ionicons name="add" size={wp(5)} color="white" />
+        </TouchableOpacity>
 
-      {modalVisible && (
-        <WorkoutModal
-          modalVisible={modalVisible}
-          setModalVisible={setModalVisible}
-        />
-      )}
-
-      {workoutList.length > 0 && (
-        <ScrollView style={{ paddingVertical: hp(15) }}>
-          <DraggableList
-            data={workoutList}
-            onReordered={(updatedData: Workout[]) => {
-              // console.log("Updated order:", updatedData);
-              dispatch(setWorkoutOrder(updatedData));
-            }}
+        {modalVisible && (
+          <WorkoutModal
+            modalVisible={modalVisible}
+            setModalVisible={setModalVisible}
           />
-        </ScrollView>
-      )}
-    </View>
+        )}
+
+        {workoutList.length > 0 && (
+          <ScrollView style={{ paddingVertical: hp(15) }}>
+            <DraggableList
+              data={workoutList}
+              onReordered={(updatedData: Workout[]) => {
+                // console.log("Updated order:", updatedData);
+                dispatch(setWorkoutOrder(updatedData));
+              }}
+            />
+          </ScrollView>
+        )}
+      </View>
+    </SafeAreaView>
   );
 }
 
